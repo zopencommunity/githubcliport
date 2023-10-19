5eeb2f46150f67d7c81b0dcfc5f1193ec1d157ad
Mike Fulton <mikefultonpersonal@gmail.com>
2023-10-19 19:01:08 -0400

    base definition

---
+++ README.md
@@ -0,0 +1,3 @@
+cli
+
+cli is GitHub on the command line. It brings pull requests, issues, and other GitHub concepts to the terminal next to where you are already working with git and your code.
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/cliport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cliport.git'), string(name: 'PORT_DESCRIPTION', value: 'cli is GitHub on the command line. It brings pull requests, issues, and other GitHub concepts to the terminal next to where you are already working with git and your code.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
