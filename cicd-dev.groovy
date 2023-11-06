fd1bd96d0d6eafd1f8d2c0ca54e2f77f57d5ec9e
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-11-06 15:49:51 -0500

    Update cicd-dev.groovy

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/cliport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cliport.git'), string(name: 'PORT_DESCRIPTION', value: 'cli is GitHub on the command line. It brings pull requests, issues, and other GitHub concepts to the terminal next to where you are already working with git and your code.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cliport.git'), string(name: 'PORT_DESCRIPTION', value: 'cli is GitHub on the command line. It brings pull requests, issues, and other GitHub concepts to the terminal next to where you are already working with git and your code.' ), string(name: 'BUILD_LINE', value: 'DEV'), string(name: 'NODE_LABEL', value: "go_120" ]
   }
 }
